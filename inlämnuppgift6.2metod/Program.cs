1303ae896a3f4aa1ab6ecc7a0c3f95383f79dd80
Admin <Admin@TEB12707>
2023-02-10 10:28:14 +0100

    Add project files.

---
+++ inlämnuppgift6.2metod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace uppgift6
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Störst(25,32));
+        }
+        static int Störst(int tal1,int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+            else if (tal1 == tal2)
+            {
+                return tal1 + tal2;
+            }
+            else
+            {
+                return tal2;
+            }
+        }
+    }
+}
